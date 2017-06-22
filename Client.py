b86751af79fab2b8f21dd12cce1f8fc2b10eef32
minfy <602221422@qq.com>
2017-06-22 22:32:18 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,37 +5,39 @@ host=socket.gethostname()
 
 addr=(host,7567)
 
-def conn():		#定义一个函数
-    t=socket.socket()#创建一个socket以连接服务器：socket = socket.socket(family, type) 
-    t.connect(addr)#.使用socket的connect方法连接服务器
-    return t
+def conn():
+    s=socket.socket()#创建一个socket以连接服务器：socket = socket.socket(family, type) 
+    s.connect(addr)#.使用socket的connect方法连接服务器
+    return s
 
-def lis(t):
-    my=[t]
+def lis(s):
+    my=[s]
     while True:
         r,w,e=select.select(my,[],[])   #异步socket处理方法
-        if t in r:
+        if s in r:
             try:
-                print t.recv(1024)
+                print s.recv(1024)
             except socket.error:
+                print 'socket is error'
                 exit()
             
-def talk(t):
+def talk(s):
     while True:
         try:
-            i=raw_input()
+            info=raw_input()
         except Exception,e:
+            print "can't input"
             exit()
         try:
-            t.send(i)
+            s.send(info)
         except Exception,e:
             print e
             exit()
             
 def main():
-    chat=conn()
-    threading.Thread(target=lis,args=(chat,)).start()
-    threading.Thread(target=talk,args=(chat,)).start()
+    ss=conn()
+    threading.Thread(target=lis,args=(ss,)).start()
+    threading.Thread(target=talk,args=(ss,)).start()
 #把函数对象lis,talk作为参数传给它的初始化函数，再调用Thread对象的start方法，线程启动后将执行此函数。
 if __name__=='__main__':
     main()
